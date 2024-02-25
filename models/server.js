252b0627337a21f5ba87883d0de638eece60836e
jmatias-2019152 <jmatias-2019152@kinal.edu.gt>
2024-02-24 22:29:42 -0600

    Subida proyecto

---
+++ models/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const { dbConnection } = require('../db/config');
+
+class Server{
+
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+
+        this.conectarDB();
+
+    
+    }
+
+    async conectarDB(){
+        await dbConnection();
+    }
+    
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log('Servidor ejecutado y escuchando en el puerto', this.port);
+        });
+    }
+}
+
+
+module.exports = Server;
